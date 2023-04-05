802ff9604852966de7b711b5464884f9a2173228
Jen B <jenbutler984@gmail.com>
2023-04-04 21:47:12 -0400

    Lots of changes, I was working on/off and neglected commits till now

--- README.md
+++ README.md
@@ -1,9 +1,75 @@
 # password_generator
 
-Random password tutorial help credit goes to [dev.to](https://dev.to/code_mystery/random-password-generator-using-javascript-6a)
+## Description
 
-Min max slider tutorial credit goes to [Foolish Developer](https://foolishdeveloper.com/how-to-create-javascript-range-slider-with-min-and-max/)
+As a technology professional with over a decade of experience, I understand the importance of online security, and creating strong, unique passwords is a crucial aspect of that.
 
-Additional tutorial help for both items to [Andy Leverenz](https://webdesign.tutsplus.com/tutorials/build-a-configurable-random-password-generator-with-javascript--cms-93262)
+I wanted to create this password generator to make it easier for myself and others to generate secure passwords, and by offering customization options the generator allows users to create passwords that suit their needs and preferences, while maintaining a high level of security.
 
+Working on this project has allowed me to strengthen my programming skills in HTML, CSS, and JavaScript, and develop a better understanding of web development and user experience design.
+
+## Installation
+
+No installation required, but if you want to run this locally:
+
+### Requirements
+
+- [Visual Studio Code](https://code.visualstudio.com/download)
+
+- [Live Server Extension](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+
+## Usage
+
+1. You will require an IDE such as Visual Studio Code. 
+
+2. After installing Visual Studio Code, install the Live Server extension.
+
+3. Download the project code and export it to your local machine.
+
+4. Open the folder where index.html resides in Visual Studio Code.
+
+5. Right-click index.html and choose "Open with Live Server".
+
+6. Click the Generate Password button to begin generating a new password.
+
+## Preview
+
+[Link to site](https://justjenb.github.io/portfolio/)
+
+![Screenshot of password generator](./assets/images/screenshot.png)
+
+## Credits
+
+- Random password tutorial help credit goes to [dev.to](https://dev.to/code_mystery/random-password-generator-using-javascript-6a)
+
+- More random password gen help credit goes to [Nico Zerpa](https://dev.to/nicozerpa/never-use-mathrandom-to-create-passwords-in-javascript-3i9j)
+
+- Using crypto.getRandomValues [Paul S.](https://stackoverflow.com/a/27181089)
+
+- Document on .push [Borislav Hadzhiev](https://bobbyhadz.com/blog/javascript-array-push-if-not-exist)
+
+- Min max slider tutorial credit goes to [Foolish Developer](https://foolishdeveloper.com/how-to-create-javascript-range-slider-with-min-and-max/)
+
+- Additional tutorial help for both items to [Andy Leverenz](https://webdesign.tutsplus.com/tutorials/build-a-configurable-random-password-generator-with-javascript--cms-93262)
+
+- More additional tutorial help on all JS items to [DevelopedByEd](https://www.youtube.com/watch?v=bbnkAV12Tig&t=654s_)
+
+- Generate random string in JavaScript for lowercase and uppercase [ropsiu on StackOverflow](https://stackoverflow.com/a/14276615)
+
+- Arrow function expressions [Mozilla](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+
+- Conditional operators [Mozilla](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator)
+
+- Clean event listeners & other tips [Ferenc Almasi](https://webtips.dev/javascript-password-generator)
+
+- More explanation on crypto.getRandomValues in an array [Reyno](https://stackoverflow.com/a/68617567)
+
+- Using Boolean as a function and filtering for true values [StackOverflow](https://stackoverflow.com/questions/29358815/using-boolean-as-the-argument-to-filter-in-javascript), [Mike Bifulco](https://mikebifulco.com/posts/javascript-filter-boolean), [Michael Uloth](https://michaeluloth.com/filter-boolean)
+
+- Shuffling an array [W3Schools](https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html)
+
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,70 +1,148 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// Assignment code here
-
+// assign elements from html page for uppercase, lowercase, numbers, special characters
 const uppercaseLetters = document.querySelector("#uppercase");
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercaseLetters = document.querySelector("#lowercase");
-// uppercaseLetters.toLowerCase();
-const numbers = document.querySelector("#numbers");
-// "1234567890";
+const numbersBox = document.querySelector("#numberbox");
 const specialCharacters = document.querySelector("#special");
-// "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-const pwMinMaxBar = document.querySelector("#pwminmaxbar");
-// "8", "132" / range
-const pwLength = document.querySelector("#pwlenthresultbox");
-// Selected
-const passwordText = document.querySelector("#password");
-// final password 
 
-pwMinMaxBar.addEventListener("change", (event) => {
-  pwLength.innerText = event.target.value;
+//  password length slider bar
+const pwLengthSlider = document.querySelector("#pwlengthslider");
+
+// assign password length slider bar
+const pwLengthResult = document.querySelector("#pwlenthresultbox");
+
+// generate password button
+const generateBtn = document.querySelector("#generate-btn");
+
+// password field
+const finalPassword = document.querySelector("#password");
+
+// copy button
+const copyPassword = document.querySelector("#copy-btn");
+
+// setting some global variables
+const copy = (text) => navigator.clipboard.writeText(text);
+let useUppercase;
+let useLowercase;
+let useNumbers;
+let useSymbols;
+let passwordLength;
+
+// basic function to shuffle an array
+function shuffleArray(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+// prompt for how long the password will be
+function promptLengthInput() {
+  let lengthInput = parseInt(
+    window.prompt("Enter password length between 8 and 128:")
+  );
+  if (lengthInput >= 8 && lengthInput <= 128) {
+    passwordLength = lengthInput;
+  } else {
+    alert(
+      "Invalid length. Password length must be between 8 and 128 characters."
+    );
+    promptLengthInput();
+  }
+}
+
+// set options array with window prompts to confirm / true/false with user input on which values to use in the password
+// using Object.values on the options array to get the length of the boolean values (how many have been chosen/which have been chosen)
+function promptPwConfigInput() {
+  const options = {
+    useUppercase: window.confirm(
+      "Do you want to include uppercase letters?\nClick OK for yes or Cancel for no."
+    ),
+    useLowercase: window.confirm(
+      "Do you want to include lowercase letters?\nClick OK for yes or Cancel for no."
+    ),
+    useNumbers: window.confirm(
+      "Do you want to include numbers?\nClick OK for yes or Cancel for no."
+    ),
+    useSymbols: window.confirm(
+      "Do you want to include symbols?\nClick OK for yes or Cancel for no."
+    ),
+  };
+
+  // calculates the number of true results in the previous options statement
+  const optionCount = Object.values(options).filter(Boolean).length;
+
+  // forces the user to pick at least two options
+  if (optionCount <= 1) {
+    alert("Please choose at least two options.");
+    return promptPwConfigInput();
+  }
+
+  // forces the user to pick uppercase or lowercase letters and not just numbers or symbols
+  if (!options.useUppercase && !options.useLowercase) {
+    alert("You must use uppercase or lowercase letters.");
+    return promptPwConfigInput();
+  }
+
+  return options;
+}
+
+// generate the new password
+function generatePassword(options) {
+  let finalPassword = "";
+
+  // converting characters from unicode using String.fromCharCode
+  const getRandomUpper = () =>
+    String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  const getRandomLower = () =>
+    String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  const getRandomNumber = () =>
+    String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  const getRandomSymbol = () =>
+    "!@#$%^&*()_+-=[]{}|;':\",.<>?/".charAt(Math.floor(Math.random() * 28));
+
+  // declaring the selectedOptions array and the push of random characters if selected
+  let selectedOptions = [];
+  if (options.useUppercase) selectedOptions.push(getRandomUpper);
+  if (options.useLowercase) selectedOptions.push(getRandomLower);
+  if (options.useNumbers) selectedOptions.push(getRandomNumber);
+  if (options.useSymbols) selectedOptions.push(getRandomSymbol);
+
+  // shuffles the initial selectedOptions array of characters, or they would come out in the same order each time
+  shuffleArray(selectedOptions);
+
+  // ensures that for each of the true selected options it gets added to the finalPassword, e.g. if upper, numbers,
+  // symbols were selected, only upper and numbers may come out if this were not configured
+  for (const option of selectedOptions) {
+    finalPassword += option();
+  }
+
+  // iterates through all of the selected options and randomly chooses one to add to the end of the finalPassword until the length of the password is reached
+  for (let i = selectedOptions.length; i < passwordLength; i++) {
+    const randomFunc =
+      selectedOptions[
+        Math.floor(
+          (crypto.getRandomValues(new Uint32Array(1))[0] / 0x100000000) *
+            selectedOptions.length
+        )
+      ];
+    finalPassword += randomFunc();
+  }
+
+  // returns the finalPassword
+  return finalPassword;
+}
+
+// copy password to the clipboard
+copyPassword.addEventListener("click", () => {
+  copy(finalPassword.value);
+  alert("Copied to clipboard.");
 });
 
-function getRandomUppercase() {
-  const upperL = "!@#$%^&*()[]=<>.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-function getRandomSymbols {
-  const symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-console.log(uppercaseLetters);
-console.log(lowercaseLetters);
-console.log(numbers);
-console.log(specialCharacters);
-
-function generatePassword() {
-
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generate button to start process
+generateBtn.addEventListener("click", () => {
+  // prompts for lengthInput
+  promptLengthInput();
+  // prompts for pwConfigInput which guides the user through option selection
+  const options = promptPwConfigInput();
+  // calls the generatePassword function with the chosen final options
+  const generatedPassword = generatePassword(options);
+  // sets the final password value
+  password.value = generatedPassword;
+});
